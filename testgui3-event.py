da5ccd5bd9bdf0611fd85c6467ce448f40be3169
root <hanulcom7@naver.com>
2019-09-09 18:49:27 +0100

    test2

---
+++ testgui3-event.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+class MyDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+ 
+        lblName = QLabel("Name")
+        self.editName = QLineEdit()
+        btnOk = QPushButton("OK")
+ 
+        layout = QVBoxLayout()
+        layout.addWidget(lblName)
+        layout.addWidget(self.editName)
+        layout.addWidget(btnOk)
+        self.setLayout(layout)
+ 
+        btnOk.clicked.connect(self.btnOkClicked)
+ 
+    def btnOkClicked(self):
+        name = self.editName.text()
+        QMessageBox.information(self, "Info", name)
